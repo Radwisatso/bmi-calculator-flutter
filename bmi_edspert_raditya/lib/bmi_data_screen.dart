a75b2e8e4c26e5830a34a165defb5e9951a53a5d
Raditya Dwisatrio Santoso <dwisatrioraditya@gmail.com>
2022-07-11 21:52:08 +0700

    Adding style on male gender

--- bmi_edspert_raditya/lib/bmi_data_screen.dart
+++ bmi_edspert_raditya/lib/bmi_data_screen.dart
@@ -21,21 +21,26 @@ class _BmiDataScreenState extends State<BmiDataScreen> {
             child: Row(
               children: [
                 Expanded(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Icon(
-                        Icons.male,
-                        size: 80,
-                      ),
-                      Text(
-                        'Male',
-                        style: TextStyle(
-                          fontSize: 18,
-                          color: Color(0xff8d8e98),
+                  child: Container(
+                    decoration: BoxDecoration(
+                        color: Colors.grey,
+                        borderRadius: BorderRadius.circular(15)),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(
+                          Icons.male,
+                          size: 80,
                         ),
-                      ),
-                    ],
+                        Text(
+                          'Male',
+                          style: TextStyle(
+                            fontSize: 18,
+                            color: Color(0xff8d8e98),
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
                 Expanded(
